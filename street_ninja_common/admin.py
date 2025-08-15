0ec5e1dfc452ac9c9d17907f8670555d2b409b3f
FirstFlush <firstflush@protonmail.com>
2025-08-14 20:13:15 -0700

    feat: add base admin module

---
+++ street_ninja_common/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.contrib.gis import admin as gis_admin
+
+
+class BaseAdmin(admin.ModelAdmin):
+    """
+    Common admin functionality for non-GIS models.
+    """
+
+    def get_readonly_fields(self, request, obj=None):
+        return [field.name for field in self.model._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class BaseGISAdmin(gis_admin.GISModelAdmin):
+    """
+    Common admin functionality for GIS models.
+    """
+
+    def get_readonly_fields(self, request, obj=None):
+        return [field.name for field in self.model._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
